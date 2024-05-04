4c3eab33f96aad029fd03b1623db71a883385da6
Nael-Nathanael <nael.nathanael71@gmail.com>
2024-05-04 15:54:08 +0700

    feat: simplify partnership

--- app/Views/_pages/dashboard/partnerships/index.php
+++ app/Views/_pages/dashboard/partnerships/index.php
@@ -42,68 +42,20 @@
 
 
     <section class="my-4 container">
-        <div class="small fw-semibold">
-            <?= summon_editable("Financial Statement Header Tag", "PARTNERSHIPS_FINS_TAG") ?>
-        </div>
-
-        <div class="bg-warning pt-1" style="max-width: 100px"></div>
-
-        <div class="h3 fw-semibold">
-            <?= summon_editable("Financial Statement Title", "PARTNERSHIPS_FINS_TITLE") ?>
-        </div>
-
-        <div class="fw-semibold">
-            <?= summon_editable("Financial Statement Subtitle", "PARTNERSHIPS_FINS_SUBTITLE") ?>
-        </div>
-
-        <div class="my-3 bg-secondary py-5 d-flex justify-content-center align-items-center">
+        <div class="my-3 bg-secondary py-5 d-flex justify-content-center align-items-center" style="min-height: 300px">
             Coming Soon: Financial Statement Editor
         </div>
     </section>
 
-    <section class="my-4 position-relative"
-             style="background: url('<?= callMedia("PARTNERSHIPS_REPORTS_BG") ?>') center;
-                     background-size: cover;"
-    >
-        <div class="position-absolute top-0 end-0 m-2">
-            <?= summon_image_button("PARTNERSHIPS_REPORTS_BG") ?>
-        </div>
-
-        <div class="container">
-            <div class="fw-semibold small">
-                <?= summon_editable("Reports Tag", "PARTNERSHIPS_REPORTS_TAG") ?>
-            </div>
-            <div class="fw-semibold h3">
-                <?= summon_editable("The Annual Reports", "PARTNERSHIPS_REPORTS_TITLE") ?>
-            </div>
-            <div class="my-3 bg-secondary py-5 d-flex justify-content-center align-items-center">
-                Coming Soon: Annual Reports Editor
-            </div>
+    <section class="my-4 container">
+        <div class="my-3 bg-secondary py-5 d-flex justify-content-center align-items-center" style="min-height: 300px">
+            Coming Soon: Annual Reports Editor
         </div>
     </section>
 
     <section class="my-4 container">
-        <div class="row">
-            <div class="col-4">
-                <div class="small fw-semibold">
-                    <?= summon_editable("Presentations Header Tag", "PARTNERSHIPS_PRESENTATIONS_TAG") ?>
-                </div>
-
-                <div class="bg-warning pt-1" style="max-width: 100px"></div>
-
-                <div class="h3 fw-semibold">
-                    <?= summon_editable("Presentations Title", "PARTNERSHIPS_PRESENTATIONS_TITLE") ?>
-                </div>
-
-                <div class="fw-semibold">
-                    <?= summon_editable("Presentations Subtitle", "PARTNERSHIPS_PRESENTATIONS_SUBTITLE") ?>
-                </div>
-            </div>
-            <div class="col-8">
-                <div class="my-3 bg-secondary py-5 d-flex justify-content-center align-items-center">
-                    Coming Soon: Presentations Editor
-                </div>
-            </div>
+        <div class="my-3 bg-secondary py-5 d-flex justify-content-center align-items-center" style="min-height: 300px">
+            Coming Soon: Presentations Editor
         </div>
     </section>
 
